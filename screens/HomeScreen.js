5dd0569a9f37efc4e9b3449b59d017557d637ad1
Tu <ituet1119@gmail.com>
2020-06-20 15:35:17 +0700

    homescreen 0.1

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { SafeAreaView, Dimensions,Image, Text, FlatList, TouchableOpacity } from 'react-native';
+import { SafeAreaView, Dimensions, Image, Text, FlatList, TouchableOpacity } from 'react-native';
 import User from '../User';
 import firebase from 'firebase';
 import { Avatar } from 'react-native-elements';
@@ -7,12 +7,14 @@ import { ListItem } from 'react-native-elements';
 // import { TextInput } from 'react-native-gesture-handler';
 // import styles from '../constants/style';
 
+import ChatScreen from './ChatScreen'
+
 export default class HomeScreen extends React.Component {
-	static navigationOptions = ({navigation})=> {
-        return {
+	static navigationOptions = ({ navigation }) => {
+		return {
 			title: 'Chats',
 			headerRight: (
-				<TouchableOpacity onPress={()=>navigation.navigate('Profile')}>
+				<TouchableOpacity onPress={() => navigation.navigate('Profile')}>
 					<Image
 						style={{ width: 32, height: 32, marginRight: 10, resizeMode: 'cover', tintColor: '#999' }}
 						source={require('../images/user.png')}
@@ -20,14 +22,14 @@ export default class HomeScreen extends React.Component {
 				</TouchableOpacity>
 			),
 		};
-    };
-
-	state = {	
-		users: [],
-		dbRef: firebase.database().ref('users'),
-		url: "https://firebasestorage.googleapis.com/v0/b/fir-chat-ffbb9.appspot.com/o/images%2Favatar%2Fdf.png?alt=media&token=c17a4c1b-f661-4e8e-80ed-4edeb56173b2"
 	};
 
+	state = {
+		users: [],
+		dbRef: firebase.database().ref('users'),
+		messageList: [],
+		dbRefMess: firebase.database().ref('messages'),
+	};
 	componentDidMount() {
 		this.state.dbRef.on('child_added', (val) => {
 			let person = val.val();
@@ -35,37 +37,51 @@ export default class HomeScreen extends React.Component {
 			var lists;
 			if (person.username === User.username) {
 				User.name = person.name;
-				// firebase
-				// 	.database()
-				// 	.ref('users/' + User.name + '/listFriend')
-				// 	.on('value', function (snap) {
-				// 		lists = snap.val();
-				// 		console.log(lists);
-				// 	});
-			}
-			else {
+			} else {
 				this.setState((prevState) => {
 					return {
 						users: [...prevState.users, person],
 					};
 				});
 			}
-
 		});
 	}
+	// getMessageLast = (user2)=>{
+	// 	this.state.dbRefMess
+	// 		.child(User.username)
+	// 		// .child(user2)
+	// 		.on('child_added', (value) => {
+	// 			this.setState((prevState) => {
+	// 				return {
+	// 					messageList: [...prevState.messageList, value.val()],
+	// 				};
+	// 			});
+	// 		});
+	// 	console.log(user2);
+	// 	console.log(this.state.messageList);
+		
+	// 	// return this.state.messageList[this.state.messageList.length- 1];
+	// 	// return this.state.messageList.length;
+	// 	return this.state.messageList.length;
+	// }
 	componentWillUnmount() {
 		this.state.dbRef.off();
 	}
-	renderRow = ({ item }) => {
-		return (
-			<TouchableOpacity
+	renderItem = ({ item }) => (
+		<TouchableOpacity
 				onPress={() => this.props.navigation.navigate('Chat', item)}
-				style={{ padding: 10, borderBottomColor: '#ccc', borderBottomWidth: 1 }}
+				style={{borderBottomColor: '#ccc'}}
 			>
-				<Text style={{ fontSize: 20 }}>{item.name}</Text>
-			</TouchableOpacity>
-		);
-	};
+			<ListItem
+				title={item.name}
+				// subtitle={this.getMessageLast(item.username)}
+				subtitle={item.username}
+				leftAvatar={{ source: { uri: item.avatar } }}
+				bottomDivider
+				chevron
+			/>
+		</TouchableOpacity>
+	)
 	_logOut = async () => {
 		await AsyncStorage.clear();
 		this.props.navigation.navigate('Auth');
@@ -74,7 +90,12 @@ export default class HomeScreen extends React.Component {
 		const { height } = Dimensions.get('window');
 		return (
 			<SafeAreaView>
-				<FlatList data={this.state.users} renderItem={this.renderRow} keyExtractor={(item) => item.username} />
+				<FlatList
+					data={this.state.users}
+					renderItem={this.renderItem}
+					keyExtractor={(item) => item.username}
+					// leftAvatar={{source :{ uri: item.avatar  } }}
+				/>
 			</SafeAreaView>
 		);
 	}
